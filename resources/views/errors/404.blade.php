896d8e9367f61160eb16c275ab2a52e9c2d91a91
dev-sief <dev.sief.hesham@gmail.com>
2021-01-31 11:57:48 +0200

    Dashboard

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Not Found</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+        <!-- Icons -->
+        <link rel="stylesheet" href="{{URL::to('assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{URL::to('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" type="text/css">
+        <!-- Argon CSS -->
+        <link rel="stylesheet" href="{{URL::to('assets/css/argon.css?v=1.2.0')}}" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="text-center">404</h1>
+                    </div>
+                    <div class="card-body text-center">
+                        <h3>Nothing Here Get Back</h3>
+                        <a href="/" class="btn btn-link">Back To Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Argon Scripts -->
+    <!-- Core -->
+    <script src="{{URL::to('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{URL::to('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{URL::to('assets/vendor/js-cookie/js.cookie.js')}}"></script>
+    <script src="{{URL::to('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js')}}"></script>
+    <script src="{{URL::to('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')}}"></script>
+    <!-- Argon JS -->
+    <script src="{{URL::to('assets/js/argon.js?v=1.2.0')}}"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// -----------------------------login-----------------------------------------
+// Route::get('/login', 'App\Http\Controllers\Auth\LoginController@login')->name('login');
+// Route::post('/login', 'App\Http\Controllers\Auth\LoginController@authenticate');
+Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
